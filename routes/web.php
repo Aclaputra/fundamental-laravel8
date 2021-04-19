f627814cf7ec2ea12ce6ada99ba88c97265841ff
Acla Putra <muhamadacla22@gmail.com>
2021-04-19 11:16:32 +0700

    first commit - kazi ariyan laravel 8 udemy

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    echo "THis is Home Page";
+});
+
+Route::get('/about ', function () {
+    return view('about');
+})->middleware('check');
+
+Route::get('/contact-sdhsjah-sjd', [ContactController::class, 'index'])->name('con');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
