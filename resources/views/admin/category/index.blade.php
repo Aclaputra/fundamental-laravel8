51b56d52c81527e4071f3d9910676c3b4aa85ca2
Acla Putra <muhamadacla22@gmail.com>
2021-04-21 12:10:19 +0700

    added resources/views/admin/

---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,40 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <!-- {{ __('Dashboard') }} -->
+            All Category <b></b>
+            
+        </h2>
+    </x-slot>
+ 
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <!-- <x-jet-welcome /> -->
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">SL No</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Created At</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                
+                        <tr>
+                            <th scope="row"></th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                        
+                    
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>  
+</x-app-layout>
