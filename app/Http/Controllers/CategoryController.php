22afd230ca533af1dfd4f59a27bdaa58fffe828b
Acla Putra <muhamadacla22@gmail.com>
2021-04-23 14:22:16 +0700

    Eloquent & query builder -> Added app/Http/Controllers/CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,11 +6,16 @@ use Illuminate\Http\Request;
 use App\Models\Category;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
     public function AllCat(){
-        return view('admin.category.index');
+        // $categories = Category::latest()->get();
+
+        // with Query Builder
+        $categories = DB::table('categories')->latest()->get();
+        return view('admin.category.index', compact('categories'));
     }
 
     public function AddCat(Request $request){
@@ -24,16 +29,23 @@ class CategoryController extends Controller
 
         ]);
 
-        Category::insert([
-            'category_name' => $request->category_name,
-            'user_id' => Auth::user()->id,
-            'created_at' => Carbon::now(),
-        ]);
+        // Category::insert([
+        //     'category_name' => $request->category_name,
+        //     'user_id' => Auth::user()->id,
+        //     'created_at' => Carbon::now(),
+        // ]);
 
-        // $category = new Category;
-        // $category->category_name = $request->category_name;
-        // $category->user_id = Auth::user()->id;
-        // $category->save();
+        $category = new Category;
+        $category->category_name = $request->category_name;
+        $category->user_id = Auth::user()->id;
+        $category->save();
+
+        // with Query Builder
+        // $data = array();
+        // $data['category_name'] = $request->category_name;
+        // $data['user_id'] = Auth::user()->id;
+
+        // DB::table('categories')->insert($data);
 
         return Redirect()->back()->with('success','Category Inserted Successfully');
     }
