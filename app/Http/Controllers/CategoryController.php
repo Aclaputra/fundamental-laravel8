d7e2e672c9171af8ab430c7da1ae79c3cd6fa301
Acla Putra <muhamadacla22@gmail.com>
2021-04-25 05:17:55 +0700

    eloquent ORM Edit & update -> modified app/Http/Controllers/CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,19 +10,23 @@ use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
-    public function AllCat(){
-        $categories = DB::table('categories')
-            ->join('users', 'categories.user_id','users.id')
-            ->select('categories.*','users.name')
-            ->latest()->paginate(5);
+    // Category adalah Model dibuat untuk akses salah satu table dalam database MySQL
 
-        // $categories = Category::latest()->paginate(5);
+    // AllCategories
+    public function AllCat(){
+        // $categories = DB::table('categories')
+        //     ->join('users', 'categories.user_id','users.id')
+        //     ->select('categories.*','users.name')
+        //     ->latest()->paginate(5);
+
+        $categories = Category::latest()->paginate(5);
 
         // with Query Builder
         // $categories = DB::table('categories')->latest()->paginate(5);
         return view('admin.category.index', compact('categories'));
     }
 
+    // Add Categories
     public function AddCat(Request $request){
         $validatedData = $request->validate([
             'category_name' => 'required|unique:categories|max:255',
@@ -53,5 +57,23 @@ class CategoryController extends Controller
         // DB::table('categories')->insert($data);
 
         return Redirect()->back()->with('success','Category Inserted Successfully');
+    }
+
+    // Edit Category
+    public function Edit($id) {
+        $categories = Category::find($id);
+
+        return view('admin.category.edit',compact('categories'));
+    }
+
+
+    public function Update(Request $request, $id){
+        $update = Category::find($id)->update([
+            'category_name' => $request->category_name,
+            'user_id' => Auth::user()->id
+        ]);
+
+        return Redirect()->route('all.category')->with('success','Category Updated Successfully');
+
     }
 }   
