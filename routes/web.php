bfc0efe26ec904354946a30e62df38748c0ff136
Acla Putra <muhamadacla22@gmail.com>
2021-04-25 05:15:31 +0700

    Eloquent ORM Edit & Update -> added routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,10 @@ Route::get('/contact-sdhsjah-sjd', [ContactController::class, 'index'])->name('c
 
 // Category Controller
 Route::get('/category/all', [CategoryController::class, 'AllCat'])->name('all.category');
-
 Route::post('/category/add', [CategoryController::class, 'AddCat'])->name('store.category');
+
+Route::get('/category/edit/{id}', [CategoryController::class, 'Edit']);
+Route::post('/category/update/{id}', [CategoryController::class, 'Update']);
 
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
