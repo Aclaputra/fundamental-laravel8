b21b865f0217494d498076dd5473ac41ebd80484
Acla Putra <muhamadacla22@gmail.com>
2021-04-25 20:40:58 +0700

    Soft Delete, Data Restore & ForceDelete Part 1  -> modified resources/views/admin/category/index.blade.php

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -11,6 +11,7 @@
         <!-- <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg"> -->
 
+<!-- Container 1 -->
                 <div class="container">
                     <div class="row">
                         <div class="col-md-8">
@@ -51,7 +52,7 @@
                                                 </td>
                                                 <td>
                                                     <a href="{{ url('category/edit/'.$category->id) }}" class="btn btn-info">Edit</a>
-                                                    <a href="" class="btn btn-danger">Delete</a>
+                                                    <a href="{{ url('softdelete/category/'.$category->id) }}" class="btn btn-danger">Delete</a>
                                                 </td>
                                             </tr>
                                         @endforeach
@@ -85,6 +86,90 @@
                                         </form>
                                     </div>
                                 </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+    <!-- Container 2 -->
+        <!-- Trash Part -->
+
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <div class="card">
+                            <!-- Message
+                            @if(session('success'))
+                                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                    <strong>{{ session('success') }}</strong>
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>
+                            @endif -->
+                                <div class="card-header">Trash List</div>
+                                    <!-- <x-jet-welcome /> -->
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">SL No</th>
+                                                <th scope="col">Category Name</th>
+                                                <th scope="col">User</th>
+                                                <th scope="col">Created At</th>
+                                                <th scope="col">Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        <!-- @php($i = 1) -->
+                                        @foreach($trashCat as $category)
+                                            <tr>
+                                                <th scope="row">{{ $categories->firstItem()+$loop->index }}</th>
+                                                <td>{{ $category->category_name }}</td>
+                                                <td>{{ $category->user->name }}</td>
+                                               
+                                                <td>
+                                                @if($category->created_at == NULL)
+                                                <span class="text-danger"> No Date Set</span>
+                                                @else
+                                                {{ Carbon\Carbon::parse($category->created_at)->diffforhumans() }}
+                                                @endif
+                                                </td>
+                                                <td>
+                                                    <a href="{{ url('category/edit/'.$category->id) }}" class="btn btn-info">Edit</a>
+                                                    <a href="" class="btn btn-danger">Delete</a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                        
+                                        
+
+                                        </tbody>
+                                    </table>
+                                    {{ $trashCat->links() }}
+
+                                </div>
+                            </div>
+
+                            <div class="col-md-4">
+                                <!-- <div class="card">
+                                    <div class="card-header">Add Category</div>
+                                    <div class="card-body">
+                                        <form action="{{ route('store.category') }}" method="POST">
+                                        @csrf
+                                            <div class="mb-3">
+                                                <label for="exampleInputEmail1" class="form-label">Category Name</label>
+                                                <input type="text" name="category_name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+
+                                                @error('category_name')
+                                                    <span class="text-danger"> {{ $message }} </span>
+                                                @enderror
+                                            </div>
+                                                
+                                            <button type="submit" class="btn btn-primary">Add Category</button>
+
+                                        </form>
+                                    </div>
+                                </div> -->
                             </div>
 
                         </div>
