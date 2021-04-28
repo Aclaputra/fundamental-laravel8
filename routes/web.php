4105fff526797a4fdc899ad6ca0b4f658fbac018
Acla Putra <muhamadacla22@gmail.com>
2021-04-29 02:28:42 +0700

    delete data with image -> modified routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,7 @@ Route::get('/brand/all', [BrandController::class, 'AllBrand'])->name('all.brand'
 Route::post('/brand/add', [BrandController::class, 'StoreBrand'])->name('store.brand');
 Route::get('/brand/edit/{id}', [BrandController::class, 'EditBrand']);
 Route::post('/brand/update/{id}', [BrandController::class, 'UpdateBrand']);
+Route::get('/brand/delete/{id}', [BrandController::class, 'DeleteBrand']);
 
 
 
