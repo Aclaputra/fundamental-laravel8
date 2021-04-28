944127a06484caa51acde670ac8d1eb8620fbafd
Acla Putra <muhamadacla22@gmail.com>
2021-04-28 07:06:16 +0700

    Eloquent ORM Edit,Update Data With Image & Without Image Part 2 -> modified app/Http/Controllers/BrandController.php

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -52,4 +52,49 @@ class BrandController extends Controller
         return view('admin.brand.edit', compact('brands'));
 
     }
+
+    public function UpdateBrand(Request $request, $id){
+
+        $validatedData = $request->validate([
+            'brand_name' => 'required|min:4',
+
+        ],
+        [
+            'brand_name.required' => 'Please Input Brand name Anjay',
+            'brand_image.min' => 'Brands Longer than 4 characters',
+
+        ]);
+
+        $old_image = $request->old_image;
+
+        $brand_image = $request->file('brand_image');
+
+        if($brand_image){
+            $name_gen = hexdec(uniqid());
+            $img_ext = strtolower($brand_image->getClientOriginalExtension());
+            $img_name = $name_gen.'.'.$img_ext;
+            $up_location = 'image/brand/';
+            $last_img = $up_location.$img_name;
+            $brand_image->move($up_location,$img_name);
+    
+            unlink($old_image);
+            Brand::find($id)->update([
+                'brand_name' => $request->brand_name,
+                'brand_image' => $last_img,
+                'created_at' => Carbon::now(),
+            ]);
+    
+            return Redirect()->back()->with('success', 'Brand Updated Successfully');
+        }else{
+
+            Brand::find($id)->update([
+                'brand_name' => $request->brand_name,
+                'created_at' => Carbon::now(),
+            ]);
+    
+            return Redirect()->back()->with('success', 'Brand Updated Successfully');
+        }
+
+        
+    }
 }
