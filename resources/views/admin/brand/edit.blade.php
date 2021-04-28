5d33794add1b3eabc99d22d5374ca3124644cf36
Acla Putra <muhamadacla22@gmail.com>
2021-04-28 07:06:54 +0700

    Eloquent ORM Edit,Update Data With Image & Without Image Part 2 -> modified resources/views/admin/brand/edit.blade.php

--- resources/views/admin/brand/edit.blade.php
+++ resources/views/admin/brand/edit.blade.php
@@ -6,6 +6,13 @@
             
         </h2>
     </x-slot>
+     <!-- Message -->
+     @if(session('success'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>{{ session('success') }}</strong>
+             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+     @endif
  
     <div class="py-12">
         <!-- <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -19,8 +26,9 @@
                                 <div class="card">
                                     <div class="card-header">Edit Brand</div>
                                     <div class="card-body">
-                                        <form action="{{ url('brand/update/'.$brands->id) }}" method="POST">
+                                        <form action="{{ url('brand/update/'.$brands->id) }}" method="POST" enctype="multipart/form-data">
                                         @csrf
+                                        <input type="hidden" name="old_image" value="{{ $brands->brand_image }}">
                                             <div class="mb-3">
                                                 <label for="exampleInputEmail1" class="form-label">Update Brand Name</label>
                                                 <input type="text" name="brand_name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{ $brands->brand_name }}">
